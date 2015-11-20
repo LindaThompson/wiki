4f2eb1e438da04d3ae495aeaaa8811bac4c00fce
Michael <michaelnguyen2306@gmail.com>
2015-11-20 19:27:46 +0700

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,16 +19,18 @@ from recommonmark.parser import CommonMarkParser
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
-from django.conf import settings
-import django
-django.setup()
+# from django.conf import settings
+# import django
+# django.setup()
+
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -37,7 +39,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx_http_domain',
-    'djangodocs',
+    # 'djangodocs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -136,17 +138,17 @@ html_theme = 'default'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# html_favicon = 'favicon.ico'
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = [
-	# '_static',
+html_static_path = [
+	'_static',
 # 	'_static/custom.css',
 # 	'_static/default.css',
 # 	'_static/app.js'
-# ]
+]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
