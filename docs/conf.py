d1d5d10fbdea3bc335d57d85e967c7f8df119d2b
Michael <michaelnguyen2306@gmail.com>
2015-11-20 18:45:13 +0700

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ from recommonmark.parser import CommonMarkParser
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readthedocs.settings.sqlite")
 sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
@@ -26,10 +27,21 @@ sys.path.insert(0, os.path.abspath('.'))
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
+from django.conf import settings
+
+import django
+django.setup()
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+sys.path.append(os.path.abspath('_ext'))
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx_http_domain',
+    'djangodocs',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
